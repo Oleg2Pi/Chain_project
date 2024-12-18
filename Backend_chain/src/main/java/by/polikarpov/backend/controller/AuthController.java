5e62901c2e32c5bd7c523a78ce07191b6e842b85
Oleg2Pi <polikarpovoleg115@gmail.com>
2024-12-19 02:34:12 +0300

    backend: add coockie

--- Backend_chain/src/main/java/by/polikarpov/backend/controller/AuthController.java
+++ Backend_chain/src/main/java/by/polikarpov/backend/controller/AuthController.java
@@ -4,7 +4,8 @@ import by.polikarpov.backend.bean.HttpSessionBean;
 import by.polikarpov.backend.entity.Person;
 import by.polikarpov.backend.service.PersonService;
 import jakarta.persistence.EntityNotFoundException;
-import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,7 +26,7 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Void> login(@RequestBody Map<String, String> payload) {
+    public ResponseEntity<Void> login(@RequestBody Map<String, String> payload, HttpServletResponse response) {
         String chatId = payload.get("chatId");
 
         if (chatId.isEmpty()) {
@@ -39,6 +40,12 @@ public class AuthController {
         }
 
         httpSessionBean.setChatId(Long.valueOf(chatId));
+
+        Cookie cookie = new Cookie("sessionId", Long.toString(chatId));
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setPath("/");
+        response.addCookie(cookie);
 
         return ResponseEntity.ok().build();
     }
